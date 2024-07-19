da47b1824cdb5bb9f33e57e966a37999eacd0e2e
Cho-Hsun Lu <billy1125@gmail.com>
2024-07-19 10:05:24 +0800

    更新檔案

---
+++ 03_停車遊戲/程式碼/03-1_Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speed = 0;
+
+    void Start() {
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {       //點擊滑鼠時
+            this.speed = 0.2f;                  //設定初始速度
+        }
+
+        transform.Translate(this.speed, 0, 0);  //移動
+        this.speed *= 0.98f;                    //減速
+    }
+}
---
+++ 03_停車遊戲/程式碼/03-4_Car.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    float speed = 0;
+    Vector2 startPos;
+
+    void Start() {
+    }
+
+    void Update() {
+
+        // 取得滑動長度
+        if(Input.GetMouseButtonDown(0)) {            
+            this.startPos = Input.mousePosition; // 點擊滑鼠時的座標
+        } else if(Input.GetMouseButtonUp(0)) {           
+            Vector2 endPos = Input.mousePosition;  // 放開滑鼠時的座標
+            float swipeLength = endPos.x - this.startPos.x;           
+            this.speed = swipeLength / 500.0f;  // 把滑動長度轉換成初始移動速度
+			GetComponent<AudioSource>().Play(); // 播放音效
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+    }
+}
